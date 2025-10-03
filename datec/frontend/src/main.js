ebf75b847846aefacb4faa4944a90ae294ce1e8d
Valerie Hernández Fernández <valerie.herfer@gmail.com>
2025-10-03 12:22:58 -0600

    Vue Frontend initialization Restart

--- datec/frontend/src/main.js
+++ datec/frontend/src/main.js
@@ -1,23 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
 import router from './router'
-import PrimeVue from 'primevue/config'
-import ToastService from 'primevue/toastservice'
-import ConfirmationService from 'primevue/confirmationservice'
-
-// Styles
-import './assets/main.css'
 
 const app = createApp(App)
 
-// PrimeVue configuration
-app.use(PrimeVue, {
-    ripple: true,
-    inputStyle: 'filled'
-})
-
-app.use(ToastService)
-app.use(ConfirmationService)
+app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
